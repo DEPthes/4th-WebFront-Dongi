aa7dd7a7ea20a81a9a867e2dc5532a59f0a3cf6d
OhDongI <odi0716@naver.com>
2025-05-07 18:14:05 +0900

    feat: todo 기능 구현을 위한 DOM 요소 변수 선언

---
+++ week4/script.js
@@ -0,0 +1,4 @@
+const input = document.getElementById('input-todo');
+const addBtn = document.getElementById('add-btn');
+const todoList = document.getElementById('todo-list');
+const countItem = document.getElementById('count-remaining');
