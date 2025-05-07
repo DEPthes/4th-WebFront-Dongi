a204e634692771d282bb2bfc2f16de7f0a7d23d8
OhDongI <odi0716@naver.com>
2025-05-07 20:31:23 +0900

    feat: 할 일 추가 기능 구현

--- week4/script.js
+++ week4/script.js
@@ -2,3 +2,35 @@ const input = document.getElementById('input-todo');
 const addBtn = document.getElementById('add-btn');
 const todoList = document.getElementById('todo-list');
 const countItem = document.getElementById('count-remaining');
+
+
+const todoLists = [];
+
+
+function handleAddBtnClick(e){
+    e.preventDefault();
+
+    todoLists.push(input.value);
+    todoList.innerHTML = "";
+
+    todoLists.map((item,index)=> {
+        const li = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = "❌";
+
+        const text = document.createTextNode(item);
+        li.appendChild(text);
+
+        li.appendChild(checkbox);
+        li.appendChild(deleteBtn);
+
+        todoList.appendChild(li);
+    })
+    
+    input.value="";
+}
+
+addBtn.addEventListener("click",handleAddBtnClick);
