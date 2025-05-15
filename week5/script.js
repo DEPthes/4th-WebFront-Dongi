17573638810b683d9f1012be30b1eb50064b824b
OhDongI <odi0716@naver.com>
2025-05-15 16:07:39 +0900

    feat: 날씨 검색에 필요한 DOM 요소 선택자 추가

---
+++ week5/script.js
@@ -0,0 +1,7 @@
+const input = document.getElementById('city-input');
+const weatherCard = document.getElementById('weather-card');
+const description = document.getElementById('description');
+const temperature = document.getElementById('temp');
+const location = document.getElementById('location');
+const icon = document.getElementById('icon');
+const form = document.getElementById('search-form');
