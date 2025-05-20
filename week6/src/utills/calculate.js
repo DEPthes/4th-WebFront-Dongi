47ff4d158fb1d998d8f780f66646fecc2e6f6a75
OhDongI <odi0716@naver.com>
2025-05-20 11:08:52 +0900

    feat: 사칙 연산 및 예외 처리 포함한 calculate 함수 구현

--- week6/src/utills/calculate.js
+++ week6/src/utills/calculate.js
@@ -1,1 +1,28 @@
 //연산 로직 처리
+    export default function calculate (prev, oper, curr){
+        const prevNum = Number(prev);
+        const currNum = Number(curr);
+        
+        let result;
+        
+            switch (oper){
+                case "+":
+                    result = prevNum + currNum;
+                    break;
+                case "-":
+                    result = prevNum - currNum;
+                    break;
+                case "x":
+                    result = prevNum * currNum;
+                    break;
+                case "%":
+                    if (currNum ===0){
+                        result = "0으로 나눌 수 없습니다"
+                    } else{
+                        result = prevNum / currNum; 
+                    }
+                    break;                 
+            }
+            return result;   
+    }
+    
